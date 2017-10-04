58193193ef8312917f32d29dce77c7d890d649d5
kefirski <olavursky@gmail.com>
2017-10-04 19:09:56 +0300

    Batchloading implemented

--- ptb_loader.py
+++ ptb_loader.py
@@ -1,15 +1,15 @@
-import os
-import re
 import collections
-from six.moves import cPickle
+import os
 
+import numpy as np
+from six.moves import cPickle
 
 
 class PTBLoader():
     def __init__(self, data_path='', force_preprocessing=False):
         """
         :param data_path: path to PTB data
-        :param force_preprocessing: wether to preprocess data even if it was preprocessed before
+        :param force_preprocessing: whether to preprocess data even if it was preprocessed before
         """
 
         assert isinstance(data_path, str), \
@@ -26,10 +26,11 @@ class PTBLoader():
         pad_token uses to fill tensor to fixed-size length
         '''
         self.go_token = '>'
-        self.pad_token = ''
+        self.pad_token = '_'
         self.stop_token = '<'
 
         self.data_files = [data_path + path for path in ['ptb.test.txt', 'ptb.train.txt', 'ptb.valid.txt']]
+        self.target_idx = {'test': 0, 'train': 1, 'valid': 2}
 
         self.idx_file = self.preprocessings_path + 'ptb_vocab.pkl'
         self.tensor_file = self.preprocessings_path + 'tensor.pkl'
@@ -103,5 +104,61 @@ class PTBLoader():
 
         self.num_lines = [len(target) for target in self.data]
 
+    def next_batch(self, batch_size, target):
+        """
+        :param batch_size: number of selected data elements
+        :param target: target from ['test', 'train', 'valid']
+        :param use_cuda: whether to use cuda
+        :return: target tensors
+        """
 
+        target = self.target_idx[target]
 
+        indexes = np.random.randint(self.num_lines[target], size=batch_size)
+        lines = np.array([self.data[target][idx][:] for idx in indexes])
+        del indexes
+
+        return self.construct_batches(lines)
+
+    @staticmethod
+    def sort_sequences(xs):
+        """
+        :param xs: An array of batches with length batch_size
+        :return: Sorted array of batches
+        """
+
+        argsort = np.argsort([len(batch) for batch in xs])[::-1]
+        return xs[argsort]
+
+    def construct_batches(self, lines):
+        """
+        :param lines: An list of indexes arrays
+        :return: Batches
+        """
+
+        lines = self.sort_sequences(lines)
+
+        encoder_input = [self.add_token(line, go=True, stop=True) for line in lines]
+        decoder_input = [self.add_token(line, go=True) for line in lines]
+        decoder_target = [self.add_token(line, stop=True) for line in lines]
+
+        encoder_input = self.padd_sequences(encoder_input)
+        decoder_input = self.padd_sequences(decoder_input)
+        decoder_target = self.padd_sequences(decoder_target)
+
+        return encoder_input, decoder_input, decoder_target
+
+    def padd_sequences(self, sequences):
+
+        lengths = [len(line) for line in sequences]
+        max_length = max(lengths)
+
+        return np.array([line + [self.char_to_idx[self.pad_token]] * (max_length - lengths[i])
+                         for i, line in enumerate(sequences)]), lengths
+
+    def add_token(self, line, go=False, stop=False):
+
+        go = [self.char_to_idx[self.go_token]] if go else []
+        stop = [self.char_to_idx[self.stop_token]] if stop else []
+
+        return go + line + stop
